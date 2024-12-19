efb1c109da002448f5bdfccc73b9e8bb5a9b63ea
WinMir22 <miron.kiselev111@gmail.com>
2024-12-19 18:17:42 +0300

    Update user_handlers.py

--- handlers/user_handlers.py
+++ handlers/user_handlers.py
@@ -4,7 +4,7 @@ from aiogram import Dispatcher
 from aiogram.filters import CommandStart, Command
 from filters.basefilters import IsAdmin
 from functions import add_user, get_users_money
-from handlers.admin_handlers import admin_command, start_command2, statistic_callback 
+from handlers.admin_handlers import admin_command, start_command2, statistic_callback
 from keyboards import start_command_keyboard, profile_keyboard
 
 dp = Dispatcher()
@@ -25,15 +25,15 @@ async def start_command(message: types.Message):
 
 async def profile(call: types.CallbackQuery):
     money = await get_users_money(user_id=int(call.from_user.id))
-    await call.message.answer(text=f'Выберите вариант из списка ниже. Ваш баланс: {money}', reply_markup=profile_keyboard)
-
-
-
+    await call.message.answer(
+        text=f"Выберите вариант из списка ниже. Ваш баланс: {money}",
+        reply_markup=profile_keyboard,
+    )
 
 
 def register_user_messages(dp: Dispatcher):
     dp.message.register(start_command, CommandStart())
-    dp.callback_query.register(profile, F.data == 'start')
+    dp.callback_query.register(profile, F.data == "start")
     dp.message.register(admin_command, Command("admin"), IsAdmin())
     dp.callback_query.register(statistic_callback, F.data == "statistic", IsAdmin())
     dp.callback_query.register(start_command2, F.data == "menu")
