9905b81681338baa60720954671e281c886955b5
Miron <miron.kiselev11@mail.ru>
2024-12-19 19:29:25 +0300

    fh

--- handlers/user_handlers.py
+++ handlers/user_handlers.py
@@ -4,7 +4,13 @@ from aiogram import Dispatcher
 from aiogram.filters import CommandStart, Command
 from filters.basefilters import IsAdmin
 from functions import add_user, get_users_money
-from handlers.admin_handlers import admin_command, start_command2, statistic_callback, send_callback
+from handlers.admin_handlers import (
+    admin_command,
+    start_command2,
+    statistic_callback,
+    send_callback,
+    admin_newsletter_step_2,
+)
 from keyboards import start_command_keyboard, profile_keyboard
 
 dp = Dispatcher()
@@ -36,5 +42,6 @@ def register_user_messages(dp: Dispatcher):
     dp.callback_query.register(profile, F.data == "start")
     dp.message.register(admin_command, Command("admin"), IsAdmin())
     dp.callback_query.register(statistic_callback, F.data == "statistic", IsAdmin())
-    dp.callback_query.register(send_callback, F.data == 'sendtoall', IsAdmin())
+    dp.callback_query.register(send_callback, F.data == "sendtoall", IsAdmin())
+    dp.callback_query.register()
     dp.callback_query.register(start_command2, F.data == "menu")
